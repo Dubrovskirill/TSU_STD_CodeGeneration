e0830de44250d3de3d9749878fec1e11be721b45
Dubrovskirill <zet10set11@gmail.com>
2025-05-12 20:59:14 +0700

    Создание интерфейса для фабрики

--- CodeGeneration/AbstractFactory.h
+++ CodeGeneration/AbstractFactory.h
@@ -1,4 +1,15 @@
 #ifndef ABSTRACTFACTORY_H
 #define ABSTRACTFACTORY_H
+#include "Unit.h"
+
+class AbstractFactory {
+public:
+    virtual std::shared_ptr<AbstractClassUnit> createClassUnit(const std::string& name) = 0;
+    virtual std::shared_ptr<AbstractMethodUnit> createMethodUnit(
+        const std::string& name, const std::string& returnType) = 0;
+    virtual std::shared_ptr<AbstractPrintOperatorUnit> createPrintOperatorUnit(
+        const std::string& text) = 0;
+    virtual ~AbstractFactory() = default;
+};
 
 #endif // ABSTRACTFACTORY_H
